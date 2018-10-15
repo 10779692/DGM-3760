62fe506ab15e1bf1fcb076e362bd63fe5191e36f
Jared Reed <42556502+10779692@users.noreply.github.com>
2018-10-15 14:02:54 -0600

    Create navbar.php

---
+++ project9/navbar.php
@@ -0,0 +1,21 @@
+<?php
+echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<a class="navbar-brand" href="#">Project 9</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNav">
+  <ul class="navbar-nav">
+	<li class="nav-item">
+	  <a class="nav-link" href="index.php">Group Directory <span class="sr-only">(current)</span></a>
+	</li>
+	<li class="nav-item">
+	  <a class="nav-link" href="add.php">Add User</a>
+	</li>
+	<li class="nav-item">
+	  <a class="nav-link" href="search.php">Search</a>
+	</li>
+  </ul>
+</div>
+</nav>'
+?> 
